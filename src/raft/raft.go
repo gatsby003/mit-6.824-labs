543be3e6582ce908bd228e652acdcf56d2e51abe
Ganesh Futane <63470761+gatsby003@users.noreply.github.com>
2022-09-03 14:23:50 +0530

    2A passing

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -150,36 +150,6 @@ func (rf *Raft) Snapshot(index int, snapshot []byte) {
 }
 
 //
-// example RequestVote RPC arguments structure.
-// field names must start with capital letters!
-//
-type AppendRPCArgs struct {
-	Term     int
-	LeaderId int
-}
-
-type AppendRPCReply struct {
-	Term    int
-	Success bool
-}
-
-type RequestVoteArgs struct {
-	// Your data here (2A, 2B).
-	Term        int
-	CandidateId int
-}
-
-//
-// example RequestVote RPC reply structure.
-// field names must start with capital letters!
-//
-type RequestVoteReply struct {
-	// Your data here (2A).
-	VoteGranted bool
-	Term        int
-}
-
-//
 // example RequestVote RPC handler.
 //
 func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
